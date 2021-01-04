c5ef17ac934d779f594b48749abdd8c65e20de2d
Ian Leon <iannleon@me.com>
2021-01-03 21:37:41 -0500

    Initial Commit

---
+++ DigitalNutritionFacts/DigitalNutritionFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalNutritionFactsApp.swift
+//  DigitalNutritionFacts
+//
+//  Created by Ian Leon on 1/3/21.
+//
+
+import SwiftUI
+
+@main
+struct DigitalNutritionFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
