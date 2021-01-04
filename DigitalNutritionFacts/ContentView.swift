d3c1ba3a9b880c761a632f2b1ce7fb3b075747ae
Ian Leon <iannleon@me.com>
2021-01-03 23:59:33 -0500

    Adds formatters and unit converters

--- DigitalNutritionFacts/ContentView.swift
+++ DigitalNutritionFacts/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text(Volume(cups: 30).formatted())
             .padding()
     }
 }
--- DigitalNutritionFacts/NutritionFacts.swift
+++ DigitalNutritionFacts/NutritionFacts.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
+extension Double {
+
+    func converted<T:Dimension>(fromUnit: T, to toUnit: T) -> Double {
+        return Measurement(value: self, unit: fromUnit)
+            .converted(to: toUnit).value
+    }
+
+    func toGrams(from fromUnit: UnitMass) -> Double {
+        return self.converted(fromUnit: fromUnit, to: .grams)
+    }
+    
+    func toMilliliters(from fromUnit: UnitVolume) -> Double {
+        return self.converted(fromUnit: fromUnit, to: .milliliters)
+    }
+    
+    func toMeasurement<T:Unit>(withUnit: T) -> Measurement<T> {
+        return Measurement(value: self, unit: withUnit)
+    }
+}
 
 struct Mass {
     
@@ -19,10 +38,10 @@ struct Mass {
     // MARK: - Convenience initializers
     
     init(mg milligrams: Double) {
-        self.init(g: milligrams / 1e3)
+        self.init(g: milligrams.toGrams(from: .milligrams))
     }
     init(mcg micrograms: Double) {
-        self.init(g: micrograms / 1e6)
+        self.init(g: micrograms.toGrams(from: .micrograms))
     }
     
     // MARK: - Custom operators
@@ -31,12 +50,44 @@ struct Mass {
         Mass(g: lhs.grams + rhs.grams)
     }
     
-    // TODO there is a MassFormatter (see what that does) is there a volume formatter?
+    // MARK: - Formatters
+    
+    static let formatter = MassFormatter()
+    
+    func formatted() -> String {
+        
+        return Mass.formatter.string(fromValue: grams, unit: .gram)
+    }
 }
 
 struct Volume {
-    let liters: Double
-    // TODO convenience initializers in cups
+    
+    let milliliter: Double
+    
+    init(mL: Double) {
+        milliliter = mL
+    }
+    
+    // MARK: - Convenience initializers
+    
+    init(cups: Double) {
+        self.init(mL: cups.converted(fromUnit: UnitVolume.cups, to: .milliliters))
+    }
+    
+    // MARK: - Formatters
+    
+    static var formatter:MeasurementFormatter = {
+        let mf = MeasurementFormatter()
+        mf.unitOptions = .naturalScale
+        mf.numberFormatter.maximumFractionDigits = 2
+        return mf
+    }()
+    
+    func formatted() -> String {
+        
+        return Volume.formatter.string(from: milliliter.toMeasurement(withUnit: UnitVolume.milliliters))
+    }
+    
 }
 
 struct NutritionFacts {
